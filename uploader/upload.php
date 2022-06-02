2346357715f1bcdbe3f69132cd8974216d458941
xpa <xpadev@gmail.com>
2022-06-02 22:51:39 +0900

    Fixed small images not to enlarge when resized

--- uploader/upload.php
+++ uploader/upload.php
@@ -24,7 +24,7 @@ while(file_exists(DATA_DIR."/img_".$file_id.".png")){
     $file_id = uniqid("",true);
 }
 //画像リサイズ
-list($w,$h) = get_contain_size($original_w,$original_h,1000,1000);
+list($w,$h) = get_resized_size($original_w,$original_h);
 $canvas = imagecreatetruecolor($w, $h);
 imagecopyresampled($canvas, $original_image, 0,0,0,0, $w, $h, $original_w, $original_h);
 imagepng($original_image,DATA_DIR."/img_".$file_id.".png",9);
@@ -34,20 +34,21 @@ imagedestroy($canvas);
 echo json_encode(["status"=>"success","id"=>$file_id]);
 
 /**
- * 内接サイズを計算する
+ * リサイズ後の画像サイズを計算
  * @param int $width
  * @param int $height
- * @param int $containerWidth
- * @param int $containerHeight
  * @return array
  */
-function get_contain_size($width, $height, $containerWidth, $containerHeight)
+function get_resized_size($width, $height)
 {
+    if ($width<=1000&&$height<=1000){
+        return [$width,$height];
+    }
     $ratio = $width / $height;
-    $containerRatio = $containerWidth / $containerHeight;
+    $containerRatio = 1;
     if ($ratio > $containerRatio) {
-        return [$containerWidth, intval($containerWidth / $ratio)];
+        return [1000, intval(1000 / $ratio)];
     } else {
-        return [intval($containerHeight * $ratio), $containerHeight];
+        return [intval(1000 * $ratio), 1000];
     }
 }
